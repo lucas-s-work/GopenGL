6ff4e19a0cc8ba3172ec13dbb3e24ae19b9c19f6
Lucas <lcassinator12345@yahoo.co.uk>
2019-08-31 23:57:31 +0100

    Add key and mouse polling support

--- graphics/window.go
+++ graphics/window.go
@@ -26,7 +26,48 @@ func CreateWindow(width, height int, name string) *glfw.Window {
 
 func Poll(window *glfw.Window) {
 	window.SwapBuffers()
+	pollInputs(window)
+}
+
+/*
+Input handling
+*/
+
+var (
+	KeyMap  = map[string]bool{}
+	MouseX  float64
+	MouseY  float64
+	RButton = false
+	LButton = false
+)
+
+func pollInputs(window *glfw.Window) {
 	glfw.PollEvents()
+	pollKeys(window)
+	pollMouse(window)
+}
+
+func pollKeys(window *glfw.Window) {
+	KeyMap["w"] = window.GetKey(glfw.KeyW) == glfw.Press
+	KeyMap["a"] = window.GetKey(glfw.KeyA) == glfw.Press
+	KeyMap["s"] = window.GetKey(glfw.KeyS) == glfw.Press
+	KeyMap["d"] = window.GetKey(glfw.KeyD) == glfw.Press
+}
+
+func KeyComboPressed(keys []string) bool {
+	for _, val := range keys {
+		if !KeyMap[val] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func pollMouse(window *glfw.Window) {
+	RButton = window.GetMouseButton(glfw.MouseButtonRight) == glfw.Press
+	LButton = window.GetMouseButton(glfw.MouseButtonLeft) == glfw.Press
+	MouseX, MouseY = window.GetCursorPos()
 }
 
 func checkerr(err error) {
